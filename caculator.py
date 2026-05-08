0717817f000fec2431c0d4e17233bce28d2dd34f
Demoxn <223942392+Demowastak3n@users.noreply.github.com>
2026-05-08 18:53:49 +0000

    Implement addition for multiple input numbers
    Added functionality to perform addition on multiple numbers.

--- caculator.py
+++ caculator.py
@@ -1,2 +1,13 @@
 islemler = input("Islem turunu seciniz = (+, -, *, /, **, //, %)")
-#Try
+ne_kadar = int(input("Kaç tane sayiyla bi işlem yapmak istiyorsunuz? = "))
+sayilar = []
+
+for i in range(ne_kadar):
+    sayi = int(input("Sayi giriniz = "))
+    sayilar.append(sayi)
+
+def topla(sayilar):
+    sonuc = 0
+    for sayi in sayilar:
+        sonuc += sayi
+    return sonuc
