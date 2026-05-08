b9a239eacbded17ddffaa9f3ce919a288b839708
Squarixz <hizlicinar518@gmail.com>
2026-05-08 22:00:13 +0300

    Update caculator.py

--- caculator.py
+++ caculator.py
@@ -1,11 +1,16 @@
-islemler = input("Islem turunu seciniz = (+, -, *, /, **, //, %)")
-sayimiktar = float(input("Kac tane sayi girmek istersiniz? = "))
+islemler = input("Islem turunu seciniz = (+, -, *, /, **, //, %) ")
+sayimiktar = int(input("Kac tane sayi girmek istersiniz? = "))
+
 sayilar = []
+
 for i in range(sayimiktar):
     sayi = int(input("Sayi giriniz = "))
     sayilar.append(sayi)
+
 def topla(sayilar):
     sonuc = 0
     for sayi in sayilar:
         sonuc += sayi
     return sonuc
+
+print(topla(sayilar))
