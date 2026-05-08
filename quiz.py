3a2ef80dc5bed40023cf98a9afa1da8f46796858
Squarixz <hizlicinar518@gmail.com>
2026-05-08 22:47:08 +0300

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -5,9 +5,9 @@ import time
 
 sure = 0
 
-soru1cevap = input("Turkiyenin Baskenti neresidir? = ")
+soru1 = input("Turkiyenin Baskenti neresidir? = ")
 
-if soru1cevap == "Ankara" or "ankara":
+if soru1 == "Ankara" or "ankara":
     print("Cevap dogru")
     d += 1
     skor += 10
@@ -17,22 +17,48 @@ else:
     y += 1
     skor -= 5
 
-    time.sleep(2)
-
-    print("Tebrikler!Oyunu tamamladin")
-
-    time.sleep(1.5)
-print(d)
-time.sleep(1.5)
-print(y)
-time.sleep(1.5)
-print(skor)
-
-if skor > 0:
-    print("Genel kulturun iyi")
-
-elif skor == 0:
-    print("Genel kulturun 0")
-
+soru2 = input("Avrupadaki toprak sekli en buyuk ulke neresidir? = ")
+if soru2 == "Rusya" or "rusya":
+    print("Cevap dogru")
+    d += 1
+    skor += 10
 else:
-    print("Bu testte 1 soru var,sen bu testte degilsin")
+    print("Cevap yanlis")
+    y += 1
+    skor -= 5
+
+soru3 = input("Dunyanin en yuksek dagi nerededir? = ")
+if soru3 == "Nepal" or "nepal":
+    print("Cevap dogru")
+    d += 1
+    skor += 10
+else:
+    print("Cevap yanlis")
+    y += 1
+    skor -= 5
+
+soru4 = input("Dunyanin en derin noktasinin ismi nedir? = ")
+if soru4 == "Mariana" or "mariana":
+    print("Cevap dogru")
+    d += 1
+    skor += 10
+else:
+    print("Cevap yanlis")
+    y += 1
+    skor -= 5
+
+soru5 = input("Dunyanin capi yaklasik olarak kac kmdir? = ")
+     if soru5 == "13000Km" or "13 000 Km":
+            print("Cevap dogru")
+            d += 1
+            skor += 10
+
+        elif soru5 == "13000km" or "13 000 km":
+            print("Cevap dogru")
+            d += 1
+            skor += 10
+
+        else:
+            print("Cevap yanlis")
+            y += 1
+            skor -= 5
