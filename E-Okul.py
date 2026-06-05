667c15eff6cf87720937b46b783c416c25b02471
Squarixz <hizlicinar518@gmail.com>
2026-06-05 20:17:45 +0300

    Update E-Okul.py

--- E-Okul.py
+++ E-Okul.py
@@ -9,3 +9,5 @@ ogrenci["Isim"] = input("Ogrenci adini giriniz = ")
 ogrenci["Sinav Notu 1"] = float(input("Ilk Sinav Notunu giriniz = "))
 ogrenci["Sinav Notu 2"] = float(input("Ikinci Sinav Notunu giriniz = "))
 ogrenci["Sozlu Notu"] = float(input("Sozlu Notunu giriniz = "))
+
+dersler = input("Ortalamasini Hesaplayacaginiz Dersi giriniz = (Mat,Turk,Ing,Fen,h ")
