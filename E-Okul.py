6dbb20e690cc9023cacf8150700b688d20513e83
Squarixz <hizlicinar518@gmail.com>
2026-06-05 21:09:05 +0300

    Update E-Okul.py

--- E-Okul.py
+++ E-Okul.py
@@ -10,10 +10,10 @@ ogrenci["Isim"] = input("Ogrenci adini giriniz = ")
 ogrenci["Yas"] = int(input("Ogrenci Yasini giriniz = "))
 ogrenci["Sinif"] = int(input("Ogrencinin kacinci Sinif oldugunugiriniz = "))
 
-dersler = [ ]
+dersler = []
+
 
 def Mat():
-
     Mat = {
 
         "Birinci Sinav Notu": 0,
@@ -26,8 +26,8 @@ def Mat():
     Mat["Ikinci Sinav Notu"] = int(input("Ikinci Sinav Notu = "))
     Mat["Sozlu  Notu"] = int(input("Sozlu Notu = "))
 
-def Turk():
 
+def Turk():
     Turk = {
         "Birinci Sinav Notu": 0,
         "Ikinci Sinav Notu": 0,
@@ -38,8 +38,8 @@ def Turk():
     Turk["Ikinci Sinav Notu"] = int(input("Ikinci Sinav Notu = "))
     Turk["Sozlu  Notu"] = int(input("Sozlu Notu = "))
 
-def Fen():
 
+def Fen():
     Fen = {
 
         "Birinci Sinav Notu": 0,
@@ -52,8 +52,8 @@ def Fen():
     Fen["Ikinci Sinav Notu"] = int(input("Ikinci Sinav Notu = "))
     Fen["Sozlu  Notu"] = int(input("Sozlu Notu = "))
 
-def Ing():
 
+def Ing():
     Ing = {
         "Birinci Sinav Notu": 0,
         "Ikinci Sinav Notu": 0,
@@ -65,29 +65,36 @@ def Ing():
     Ing["Sozlu  Notu"] = int(input("Sozlu Notu = "))
 
 def ort():
-    secim = input("Hangi dersin ortalamasini hesaplamak istiyorsunuz? (Mat, Turk, Fen, Ing, Genel) = ")
-    
+
+    secim = input("Hangi dersin ortalamasini hesaplamak istiyorsunuz? (MatO, TurkO, FenO, IngO ) = ")
+
     Mat_ort = ((Mat["Birinci Sinav Notu"] * 2) + (Mat["Ikinci Sinav Notu"] * 2) + Mat["Sozlu  Notu"]) / 5
-    
+
     Turk_ort = ((Turk["Birinci Sinav Notu"] * 2) + (Turk["Ikinci Sinav Notu"] * 2) + Turk["Sozlu  Notu"]) / 5
-    
+
     Fen_ort = ((Fen["Birinci Sinav Notu"] * 2) + (Fen["Ikinci Sinav Notu"] * 2) + Fen["Sozlu  Notu"]) / 5
-    
+
     Ing_ort = ((Ing["Birinci Sinav Notu"] * 2) + (Ing["Ikinci Sinav Notu"] * 2) + Ing["Sozlu  Notu"]) / 5
-    
+
     Ort = ((Mat_ort * 3) + (Turk_ort * 2) + (Fen_ort * 2) + (Ing_ort * 1)) / 8
 
-    if secim == "Mat":
-        print(f"Matematik Ortalamaniz = {Mat_ort}")
-    elif secim == "Turk":
-        print(f"Turkce Ortalamaniz = {Turk_ort}")
-    elif secim == "Fen":
-        print(f"Fen Ortalamaniz = {Fen_ort}")
-    elif secim == "Ing":
-        print(f"Ingilizce Ortalamaniz = {Ing_ort}")
-    elif secim == "Genel":
-        print(f"Genel Ortalamaniz = {Ort}")
-    else:
-        print("Gecersiz secim, lutfen tekrar deneyiniz.")
-        ort()
+    if secim == "MatO":
+         Mat()
+         print(ogrenci["Isim"], Mat_ort, "Aldi")
 
+    elif secim == "TurkO":
+        Turk()
+        print(ogrenci["Isim"], Turk_ort, "Aldi")
+
+    elif secim == "FenO":
+        Fen()
+        print(ogrenci["Isim"], Fen_ort, "Aldi")
+
+    elif secim == "IngO":
+        Ing()
+        print(ogrenci["Isim"], Ing_ort, "Aldi")
+
+    else:
+        print(ogrenci["Isim"], Ort, "Aldi")
+    
+ort()
