8786dcd98e453e842ca9f728009df7d852a071fb
Squarixz <hizlicinar518@gmail.com>
2026-06-05 20:28:57 +0300

    Update E-Okul.py

--- E-Okul.py
+++ E-Okul.py
@@ -1,9 +1,9 @@
 ogrenci = {
-    
+
     "Isim": "",
     "Yas": 0,
     "Sinif": 0,
-    
+
 }
 
 ogrenci["Isim"] = input("Ogrenci adini giriniz = ")
@@ -12,24 +12,28 @@ ogrenci["Sinif"] = int(input("Ogrencinin kacinci Sinif oldugunugiriniz = "))
 
 dersler = [ ]
 
-Mat = {
+def Mat():
     
-    "Birinci Sinav Notu": 0,
-    "Ikinci Sinav Notu": 0,
-    "Sozlu Notu": 0,
+    Mat = {
+
+        "Birinci Sinav Notu": 0,
+        "Ikinci Sinav Notu": 0,
+        "Sozlu Notu": 0,
+
+    }
+
+    Mat["Birinci Sinav Notu"] = int(input("Birinci Sinav Notu = "))
+    Mat["Ikinci Sinav Notu"] = int(input("Ikinci Sinav Notu = "))
+    Mat["Sozlu  Notu"] = int(input("Sozlu Notu = "))
+
+def Turk():
     
-}
+    Turk = {
+        "Birinci Sinav Notu": 0,
+        "Ikinci Sinav Notu": 0,
+        "Sozlu Notu": 0,
+    }
 
-Mat["Birinci Sinav Notu"] = int(input("Birinci Sinav Notu = "))
-Mat["Ikinci Sinav Notu"] = int(input("Ikinci Sinav Notu = "))
-Mat["Sozlu  Notu"] = int(input("Sozlu Notu = "))
-
-Turk ={
-    "Birinci Sinav Notu": 0,
-    "Ikinci Sinav Notu": 0,
-    "Sozlu Notu": 0,
-}
-
-Turk["Birinci Sinav Notu"] = int(input("Birinci Sinav Notu = "))
-Turk["Ikinci Sinav Notu"] = int(input("Ikinci Sinav Notu = "))
-Turk["Sozlu  Notu"] = int(input("Sozlu Notu = "))
+    Turk["Birinci Sinav Notu"] = int(input("Birinci Sinav Notu = "))
+    Turk["Ikinci Sinav Notu"] = int(input("Ikinci Sinav Notu = "))
+    Turk["Sozlu  Notu"] = int(input("Sozlu Notu = "))
